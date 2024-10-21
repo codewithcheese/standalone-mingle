527cc16ae10a0d2603b2857298d06f619b309f2f
codewithcheese <mr.tmanning@gmail.com>
2024-10-21 18:58:42 +1000

    Add example conditional wire:model rendering

--- resources/js/click/Click.svelte
+++ resources/js/click/Click.svelte
@@ -1,6 +1,8 @@
 <script>
     let { wire, data } = $props();
 
+    let showLink = $state(false);
+
     function navigate(e) {
         e.preventDefault();
         wire.navigate("/stream");
@@ -19,5 +21,18 @@
         Increment
     </button>
     <p>Count: {data.count}</p>
-    <div><a href="/stream" onclick={() => navigate()}>Go to Stream</a></div>
+    {#if showLink}
+        <input type="text" wire:model="count" />
+        <div><a href="/stream" onclick={() => navigate()}>Go to Stream</a></div>
+    {:else}
+        <button
+            onclick={() => {
+                showLink = true;
+            }}
+            class="rounded bg-white px-2 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+            type="button"
+        >
+            Show Link
+        </button>
+    {/if}
 </div>
