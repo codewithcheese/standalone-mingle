81a42d1e7fb7963a10edaea6f4de5fe91f5ba662
codewithcheese <mr.tmanning@gmail.com>
2024-10-21 16:54:18 +1000

    Add wire.navigate example in event handler

--- resources/js/click/Click.svelte
+++ resources/js/click/Click.svelte
@@ -1,15 +1,23 @@
 <script>
+    let { wire, data } = $props();
 
-    let { wire, data} = $props();
+    function navigate(e) {
+        e.preventDefault();
+        wire.navigate("/stream");
+    }
 
-    $inspect('click', data)
+    $inspect("click", data);
 </script>
 
 <div class="py-4">
     <p>Wire:click example</p>
-    <button class="rounded bg-white px-2 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" type="button" wire:click="increment">
+    <button
+        class="rounded bg-white px-2 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+        type="button"
+        wire:click="increment"
+    >
         Increment
     </button>
-    <p>Count: { data.count }</p>
+    <p>Count: {data.count}</p>
+    <div><a href="/stream" onclick={() => navigate()}>Go to Stream</a></div>
 </div>
-
